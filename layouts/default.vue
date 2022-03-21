34c3fbaefb71ccc3e66e050ec947802b39ce32f2
Muhammad-Rubel <rubel.contact@gmail.com>
2022-03-21 16:44:56 +0600

    Empty project created.

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>I am from layout</h1>
+
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-[8rem]">Test</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Post from '@/types/Post'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  layout: 'default',
+
+  data() {
+    return {
+      posts: [] as Post[],
+    }
+  },
+})
+</script>
